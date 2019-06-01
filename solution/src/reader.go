a5144cf900ced235733600171f78f2f085908b71
Michele Ciccozzi <michele@precisdigital.com>
2019-06-01 04:17:01 +0200

    Add CouchDB support

--- solution/src/reader.go
+++ solution/src/reader.go
@@ -5,6 +5,9 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-kivik/couchdb"
+	"github.com/go-kivik/kivik"
+	"golang.org/x/net/context"
 	"io"
 	"log"
 	"os"
@@ -13,24 +16,45 @@ import (
 	"time"
 )
 
-func worker(eventChan <-chan []string, doneChan <-chan bool) {
+func worker(eventChan chan []string, doneChan <-chan bool) {
 	fmt.Println("worker started")
+	client, _ := kivik.New("couch", "http://admin:pass@localhost:5984/")
+	db := client.DB(context.TODO(), "finding")
 	for {
 		select {
 		case event, _ := <-eventChan:
 			layout := "2006-01-02 15:04:05" // January 2nd :roll_eyes:
 			datetime, _ := time.Parse(layout, event[1])
+			vulnerability := static.GetVulnerability(event[2])
 			finding := static.Finding{
 				Id:            event[0],
 				Time:          datetime,
-				Vulnerability: static.Vul[event[2]],
+				Vulnerability: &vulnerability,
 			}
 
 			findingJson, _ := json.Marshal(finding)
-			fmt.Println(string(findingJson))
+			rev, err := db.Put(context.TODO(), event[0], finding)
+			if err != nil {
+				// document was existing, try to update it
+				fmt.Println("first err ", err, rev, finding)
+				row := db.Get(context.TODO(), event[0])
+				var m map[string]interface{}
+				json.Unmarshal(findingJson, &m)
+				fmt.Println(row.Rev, m)
+				m["_rev"] = row.Rev
+				findingJson, _ = json.Marshal(m)
+				rev, err := db.Put(context.TODO(), event[0], m)
+				if err != nil {
+					// update failed (probably another concurrent update), requeue the message
+					fmt.Println("second err ", err, rev, finding)
+					eventChan <- ([]string{event[0], event[1], event[2]})
+				} else {
+					fmt.Println("first done ", err, rev, finding)
+				}
+			}
 		case done, _ := <-doneChan:
 			if done {
-				fmt.Println("worker exiting ")
+				fmt.Println("worker exiting")
 				return
 			}
 		}
@@ -53,7 +77,7 @@ func main() {
 	}
 	defer file.Close()
 	reader := csv.NewReader(bufio.NewReader(file))
-	workerscounter := 0
+	workerscounter := 1
 	counter := 0
 	for {
 		finding, err := reader.Read()
@@ -70,11 +94,11 @@ func main() {
 					workerscounter += 1
 				}
 				eventChan <- finding
-				fmt.Println("added worker ", runtime.NumGoroutine())
+				fmt.Println("added worker and job")
 			}
 		} else {
 			if err == io.EOF {
-				if workerscounter > 2 {
+				if workerscounter > 1 {
 					doneChan <- true
 					workerscounter -= 1
 				}
--- solution/src/staticdata/staticdata_loader.go
+++ solution/src/staticdata/staticdata_loader.go
@@ -3,9 +3,14 @@ package staticdata
 import (
 	"bufio"
 	"encoding/csv"
+	"encoding/json"
+	_ "github.com/go-kivik/couchdb"
+	"github.com/go-kivik/kivik"
+	"golang.org/x/net/context"
 	"io"
 	"log"
 	"os"
+	"sync"
 	"time"
 )
 
@@ -40,6 +45,8 @@ func load_crowdsourcers() {
 	csvFile, _ := os.Open("input/crowdsourcer.csv")
 	reader := csv.NewReader(bufio.NewReader(csvFile))
 	Cs = make(map[string]*Crowdsourcer)
+	client, _ := kivik.New("couch", "http://admin:pass@localhost:5984/")
+	db := client.DB(context.TODO(), "crowdsourcer")
 	for {
 		line, error := reader.Read()
 		if error == io.EOF {
@@ -51,13 +58,26 @@ func load_crowdsourcers() {
 			Id: line[0],
 			Name:  line[1],
 		}
+		csJson, _ := json.Marshal(*Cs[line[0]])
+		db.Put(context.TODO(), line[0], csJson)
 	}
+}
+
+func GetCrowdsourcer(cs_id string) (Crowdsourcer) {
+	client, _ := kivik.New("couch", "http://admin:pass@localhost:5984/")
+	db := client.DB(context.TODO(), "crowdsourcer")
+	row := db.Get(context.TODO(), cs_id)
+	cs := Crowdsourcer{}
+	row.ScanDoc(&cs)
+	return cs
 }
 
 func load_severities() {
 	csvFile, _ := os.Open("input/severity.csv")
 	reader := csv.NewReader(bufio.NewReader(csvFile))
 	Sev = make(map[string]*Severity)
+	client, _ := kivik.New("couch", "http://admin:pass@localhost:5984/")
+	db := client.DB(context.TODO(), "severity")
 	for {
 		line, error := reader.Read()
 		if error == io.EOF {
@@ -69,13 +89,26 @@ func load_severities() {
 			Id: line[0],
 			Value:  line[1],
 		}
+		sevJson, _ := json.Marshal(*Sev[line[0]])
+		db.Put(context.TODO(), line[0], sevJson)
 	}
+}
+
+func GetSeverity(sev_id string) (Severity) {
+	client, _ := kivik.New("couch", "http://admin:pass@localhost:5984/")
+	db := client.DB(context.TODO(), "severity")
+	row := db.Get(context.TODO(), sev_id)
+	sev := Severity{}
+	row.ScanDoc(&sev)
+	return sev
 }
 
 func load_vulnerabilities() {
 	csvFile, _ := os.Open("input/vulnerability.csv")
 	reader := csv.NewReader(bufio.NewReader(csvFile))
 	Vul = make(map[string]*Vulnerability)
+	client, _ := kivik.New("couch", "http://admin:pass@localhost:5984/")
+	db := client.DB(context.TODO(), "vulnerability")
 	for {
 		line, error := reader.Read()
 		if error == io.EOF {
@@ -83,11 +116,26 @@ func load_vulnerabilities() {
 		} else if error != nil {
 			log.Fatal(error)
 		}
+		crowdsourcer := GetCrowdsourcer(line[1])
+		severity := GetSeverity(line[2])
 		Vul[line[0]] = &Vulnerability{
 			Id: line[0],
-			Cs:  Cs[line[1]],
-			Sev: Sev[line[2]],
+			Cs:  &crowdsourcer,
+			Sev: &severity,
 		}
+		vulJson, _ := json.Marshal(*Vul[line[0]])
+		db.Put(context.TODO(), line[0], vulJson)
+	}
+}
+
+func GetVulnerability(vul_id string) (Vulnerability) {
+	client, _ := kivik.New("couch", "http://admin:pass@localhost:5984/")
+	db := client.DB(context.TODO(), "vulnerability")
+	row := db.Get(context.TODO(), vul_id)
+	vul := Vulnerability{}
+	row.ScanDoc(&vul)
+	return vul
+}
 	}
 }
 
